9baeda35e30931d48b1aea8e594876a9a00b14a2
moon-jun <magte007@naver.com>
2024-01-06 20:03:05 +0900

    * 커스터마이징 상단바 셋업을 위해 index,main preload파일 수정

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
-            webviewTag: true // WebView 사용을 위해 필요
+            contextIsolation: true, // 컨텍스트 격리 활성화
+            nodeIntegration: false, // Node.js 통합 비활성화
+            webviewTag: true, // WebView 사용을 위해 필요
+            preload: path.join(__dirname, 'preload.js') // preload 스크립트 지정
         }
     });
 
@@ -21,4 +25,21 @@ app.on('window-all-closed', function () {
 
 app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+});
+
+ipcMain.on('minimize', (event) => {
+    BrowserWindow.getFocusedWindow().minimize();
+});
+
+ipcMain.on('maximize', (event) => {
+    const window = BrowserWindow.getFocusedWindow();
+    if (window.isMaximized()) {
+        window.unmaximize();
+    } else {
+        window.maximize();
+    }
+});
+
+ipcMain.on('close', (event) => {
+    BrowserWindow.getFocusedWindow().close();
+});
